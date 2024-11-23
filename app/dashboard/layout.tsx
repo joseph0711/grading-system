ed6a02a11fe82b83b2bef1446b051536c6923c66
Joseph Chiang <joseph900711@gmail.com>
2024-11-23 22:30:30 +0800

    Refactor DashboardLayout component to add a background gradient

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 dark:from-gray-900 dark:to-gray-800">
+      {children}
+    </div>
+  );
+}
