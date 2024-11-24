d651fb6ce12e7577a7c747709216ab36e6613487
Joseph Chiang <joseph900711@gmail.com>
2024-11-24 21:34:34 +0800

    Refactor layout.tsx to add AuthLayout component

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+      <div className="pt-16">{children}</div>
+    </div>
+  );
+}
